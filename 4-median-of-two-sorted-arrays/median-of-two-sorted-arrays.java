339a2c0c2dc489f06ff10794fc82c131f9239036
digitaldodo <159223493+digitaldodo@users.noreply.github.com>
2025-09-27 10:19:41 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (12.74%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,29 +1,29 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) { 
-        if(nums1.length>nums2.length){
-            return findMedianSortedArrays(nums2,nums1);
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+          if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
         }
-        int n=nums1.length,m=nums2.length;
-        int l=0,r=nums1.length;
+        int m=nums1.length,n=nums2.length;
+        int l=0,r=m;
         while(l<=r){
-            int Px=l+(r-l)/2;
-            int Py=(m+n+1)/2-Px;
-            int x1 = (Px==0) ? Integer.MIN_VALUE : nums1[Px-1];
-            int x2 = (Py==0) ? Integer.MIN_VALUE : nums2[Py-1];
-            int x3 = (Px==n) ? Integer.MAX_VALUE : nums1[Px];
-            int x4 = (Py==m) ? Integer.MAX_VALUE : nums2[Py];
+            int P1=l+(r-l)/2;
+            int P2=(m+n+1)/2 -P1;
+            int x1= (P1==0) ? Integer.MIN_VALUE : nums1[P1-1];
+            int x2= (P2==0) ? Integer.MIN_VALUE : nums2[P2-1];
+            int x3= (P1==m) ? Integer.MAX_VALUE : nums1[P1];
+            int x4= (P2==n) ? Integer.MAX_VALUE : nums2[P2];
+
             if(x1<=x4 && x2<=x3){
                 if((m+n)%2==1){
                     return Math.max(x1,x2);
                 }
-            
-            return (Math.max(x1,x2)+Math.min(x3,x4))/2.0;
+                return (Math.max(x1,x2)+Math.min(x3,x4))/2.0;
             }
             else if (x1>x4){
-                r=Px-1;
+                r=P1-1;
             }
             else if (x2>x3){
-                l=Px+1;
+                l=P1+1;
             }
         }
         return -1;
